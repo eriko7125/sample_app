471e8e0fd5d0104a5ff024bf7b4c15066efe557f
髙橋恵理子 <eriko@erikonoMacBook-Pro.local>
2019-12-17 12:22:30 +0900

    Rails app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '5e668a2aab0057126ceef1cdcdf6760b51f0e08570617f0ea08770778327e9f545583defa1b8582018468ae0e2e0dbe51b951203a20d5438ca6a9ab6b2b79088'
